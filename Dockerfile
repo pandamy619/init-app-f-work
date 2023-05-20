d4ea79781cd1166e56c25fe000213ecf28e67d9e
Влад Тагаев <vladislavtagaev@gmail.com>
2023-05-21 00:32:16 +0300

    fix: dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,17 @@ FROM ubuntu:22.04
 RUN echo 'APT::Install-Suggests "0";' >> /etc/apt/apt.conf.d/00-docker
 RUN echo 'APT::Install-Recommends "0";' >> /etc/apt/apt.conf.d/00-docker
 # Preventing prompt errors during package installation
-RUN DEBIAN_FRONTEND=noninteractive \
-  apt-get update \
-  && apt-get install -y python3 \
-  && rm -rf /var/lib/apt/lists/*
+RUN DEBIAN_FRONTEND=noninteractive
+# Run as non-root user
+# RUN useradd -ms /bin/bash apprunner
+# USER apprunner
 
-RUN useradd -ms /bin/bash apprunner
+# Run as root user
+USER root
 
-USER apprunner
+WORKDIR /app
+
+COPY ./init_work.sh app/init_work.sh
+# RUN chmod +x app/init_work.sh
+# RUN ["chmod", "+x", "./init_work.sh"]
+RUN ["/bin/bash", "-c", "app/init_work.sh"]
--- init_work.sh
+++ init_work.sh
@@ -6,40 +6,40 @@ printf "START INIT\n"
 
 printf "Update && Upgrade\n"
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 
 if ! git --version &> /dev/null
 then
     printf "Install git\n"
-    apt-get install git
+    apt-get install git -y
 fi
 
-if ! htop -v &> /dev/null
-then 
-    printf "Install htop"
-    apt-get install htop
-fi
+# if ! htop -v &> /dev/null
+# then 
+#    printf "Install htop"
+#    apt-get install htop
+# fi
 
-if ! zsh --version &> /dev/null
-then 
-    printf "Install zsh\n"
-    apt-get install zsh
+# if ! zsh --version &> /dev/null
+# then 
+#    printf "Install zsh\n"
+#    apt-get install zsh
     
-    printf "default shell zsh\n"
-    chsh -s $(which zsh)
-fi
+#    printf "default shell zsh\n"
+#    chsh -s $(which zsh)
+# fi
 
-if ! curl -V &> /dev/null
-then
-    printf "Install curl\n"
-    apt-get install curl
-fi
+# if ! curl -V &> /dev/null
+# then
+#    printf "Install curl\n"
+#    apt-get install curl
+# fi
 
 
-if [ ! -d "/root/.oh-my-zsh" ]
-then
-    printf "install oh-my-zsh"  
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
+# if [ ! -d "/root/.oh-my-zsh" ]
+# then
+#    printf "install oh-my-zsh"  
+#    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# fi
 
-apt install python3-neovim
+# apt install python3-neovim
