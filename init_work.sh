a9cbbbca95d6cc05979949db495c8e588d43621b
Влад Тагаев <vladislavtagaev@gmail.com>
2023-06-18 17:56:55 +0400

    feat: check os / dist version

--- init_work.sh
+++ init_work.sh
@@ -2,33 +2,66 @@
 
 now=$(date '+%Y/%m/%d %H:%M:%S')
 
+isNotSupported() {
+  echo "System is not supported"
+  exit 1;
+}
+
+updateUbuntu() {
+  printf "Update && Upgrade\n"
+  apt-get update
+  apt-get upgrade -y
+  apt-get install software-properties-common -y
+  add-apt-repository ppa:deadsnakes/ppa
+}
+
 printf "START INIT\n"
 
-printf "Update && Upgrade\n"
-apt-get update
-apt-get upgrade -y
-
-apt-get install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa
+# Check that the system is supported
+printf "Check that the system is supported\n"
+platform='unknown'
+uname_str=$(uname)
+if [ "$uname_str" == "Linux" ];
+then
+  linux_distribution=$(grep -E '^(NAME)=' /etc/os-release)
+  if [ "$linux_distribution" == 'NAME="Ubuntu"' ];
+  then 
+    updateUbuntu
+  else
+    isNotSupported
+  fi
+  sleep 5
+else
+  isNotSupported
+fi
 
 filename='./config/package.txt'
-n=1
-while read line; do
 
-  fields=($(printf "%s" "$line"|cut -d'=' --output-delimiter=' ' -f1-))
-  if [ -z "${fields[1]}" ]
-  then
-      echo "Package No. $n package: ${fields[0]} Empty version"
-      sleep 5
-      apt-get install ${fields} -y
-  else
-      echo "Package No. $n package: ${fields[0]} version: ${fields[1]}"
-      sleep 5
-      apt-get install  ${fields[0]}${fields[1]} -y
-  fi
+# check config file exist
+if [ ! -f "$filename" ]; 
+then 
+  echo "file config 'package.txt' not exist"
+  exit 1;
+fi
 
-  n=$((n+1))
-done < $filename
+
+# n=1
+# while read line; do
+
+#  fields=($(printf "%s" "$line"|cut -d'=' --output-delimiter=' ' -f1-))
+#  if [ -z "${fields[1]}" ]
+#  then
+#      echo "Package No. $n package: ${fields[0]} Empty version"
+#      sleep 5
+#      apt-get install ${fields} -y
+#  else
+#      echo "Package No. $n package: ${fields[0]} version: ${fields[1]}"
+#      sleep 5
+#      apt-get install  ${fields[0]}${fields[1]} -y
+#  fi
+#
+#  n=$((n+1))
+# done < $filename
 
 # if ! git --version &> /dev/null
 # then
